e089eb65fc3c4cb356a48d447454b8d276e9d631
Jacob Enon <jacobenon@hotmail.com>
2020-07-09 10:51:34 +0100

    Initial Commit

---
+++ ClassTemplates/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BigJacob.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTemplates
+{
+    public class ViewModel : BaseViewModel
+    {
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
